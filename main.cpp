b50b98d3dbf607ca50f39b374c12f07f96167694
Ant0CPU <antonio.bruno@marconibari.edu.it>
2024-09-30 15:31:27 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,34 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    float num1, num2, num3, massimo;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    cout<<"Inserisci tre numeri"<<endl;
+    cin>>num1;
+    cin>>num2;
+    cin>>num3;
+
+    if (num1>num2)
+    {
+        if (num1>num3)
+        {
+            massimo=num1;
+        }
+        else
+        {
+            massimo=num3;
+        }
+    }
+    else
+    {
+        if (num2>num3)
+        {
+            massimo=num2;
+        }
+        else
+        {
+            massimo=num3;
+        }
+    }
+    cout<<"Il maggiore è " <<massimo<<endl;
+}
